4e2e446388ba79d5ce469139618a54d5d23df792
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-09 00:10:55 +0200

    Upgation Enumerate, practice

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ names = ["Rachael Green", "Goodfellow Ian", "Tedd Crock", "Mina Joseph"]
 salaries = [10260, 41571, 71211, 52141, 35781]
 people_salaries = []
 for i, j in enumerate(names):
-    x=j+ "§" + str(salaries[i])
+    x=j+ " § " + str(salaries[i])
     people_salaries.append(x)
 print(people_salaries)\
 # List comprehension :

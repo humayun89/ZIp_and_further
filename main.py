1ba0d32cf35c0cd53ff328d3cd73fe3e5a52efd2
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-08 14:20:02 +0200

    Upgation Enumerate, practice

--- main.py
+++ main.py
@@ -18,7 +18,16 @@ names = ["Rachael Green", "Goodfellow Ian", "Tedd Crock", "Mina Joseph"]
 salaries = [10260, 41571, 71211, 52141, 35781]
 people_salaries = []
 
-for i, j in enumerate(names):
+for i, j in enumerate(names): ***** Here  j in enumerate(names) implies the j variable is related to names****
     x = j + " $" + str(salaries[i])
+    people_salaries.append(x)
+print(people_salaries)
+
+# upgration of last one:
+names = ["Rachael Green", "Goodfellow Ian", "Tedd Crock", "Mina Joseph"]
+salaries = [10260, 41571, 71211, 52141, 35781]
+people_salaries = []
+for i, j in enumerate(names):
+    x=j+ "§" + str(salaries[i])
     people_salaries.append(x)
 print(people_salaries)
